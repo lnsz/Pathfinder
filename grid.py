0d65ff10ff37a28ee812615291b9ebf3abdbfa54
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2016-01-04 03:32:56 -0500

    Update grid.py

--- grid.py
+++ grid.py
@@ -2,18 +2,18 @@ import tile as t
 
 
 class Grid:
-    def __init__(self, length, height, start, end):
+    def __init__(self, window, length, height, start, end):
         self.length = length
         self.height = height
         self.start = (start[0], start[1])
         self.end = (end[0], end[1])
         self.tiles = []
-        for x in range(length):
+        for y in range(height):
             self.tiles.append([])
-            for y in range(height):
-                self.tiles[x].append(t.Tile(x, y))
-        self.tiles[start[1]][start[0]].set_state("start")
-        self.tiles[end[1]][end[0]].set_state("end")
+            for x in range(length):
+                self.tiles[y].append(t.Tile(x, y, window.getWidth() // self.length, window.getHeight() // height))
+        self.tiles[start[1]][start[0]].set_value("S")
+        self.tiles[end[1]][end[0]].set_value("E")
 
     def __str__(self):
         s = ""
@@ -25,6 +25,26 @@ class Grid:
         return s
 
     def get_length(self):
+        return self.length
+
+    def get_height(self):
+        return self.height
+
+    def get_start(self):
+        return self.start
+
+    def get_end(self):
+        return self.start
+
+    def tile_at(self, x, y):
+        return self.tiles[y][x].get_value()
+
+    def modify_tile(self, x, y, value):
+        self.tiles[y][x].set_value(value)
+
+    def get_tiles(self):
+        return self.tiles
+
         return self.length
 
     def get_height(self):
