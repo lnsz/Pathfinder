5e308a8b596c3dfdcf412774ed7d423ebe576912
lucaslunasouza <lucas.lunasouza@mail.utoronto.ca>
2016-01-04 21:08:07 -0500

    Update grid.py

--- grid.py
+++ grid.py
@@ -11,9 +11,13 @@ class Grid:
         for y in range(height):
             self.tiles.append([])
             for x in range(length):
-                self.tiles[y].append(t.Tile(x, y, window.getWidth() // self.length, window.getHeight() // height))
+                self.tiles[y].append(t.Tile(x, y, window.getWidth() //
+                                            self.length, window.getHeight() //
+                                            height))
         self.tiles[start[1]][start[0]].set_value("S")
         self.tiles[end[1]][end[0]].set_value("E")
+        print("Initialized {0} x {1} grid. Start: {2}. End: {3}.".
+              format(length, height, start, end), end="")
 
     def __str__(self):
         s = ""
@@ -48,7 +52,7 @@ class Grid:
     def get_tiles(self):
         return self.tiles
 
-    def clean_grid(self, empty, start, end):
+    def clean_grid(self, empty):
         for x in self.tiles:
             for y in x:
                 if type(y.get_value()) == int:
